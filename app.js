d8cfd5f3ee228e239455a1d5502646e9f5396569
djkaz <justbekazu@gmail.com>
2021-04-08 09:59:11 -0500

    adding new nodes

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("hello Node!");
+const profileDateArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDateArgs);
+
+
+// notice the lack of parantheses around the "profileDataArr" parameter?
+
+const printProfileData = profileDataArr => {
+    for (let i =0; i < profileDataArr.length; i++) {
+
+    
+    
+    console.log(profileDataArr[i]);
+}
+};
+
+printProfileData(profileDateArgs);
