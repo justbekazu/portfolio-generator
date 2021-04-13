bafc7388700f799c0d6b94f3d266a6378138754f
djkaz <justbekazu@gmail.com>
2021-04-12 20:59:53 -0500

    added a seperate js for functions

--- app.js
+++ app.js
@@ -1,16 +1,94 @@
-const profileDateArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDateArgs);
+// const fs = require('fs'); 
+ 
+// const profileDateArgs = process.argv.slice(2, process.argv.length);
+// // console.log(profileDateArgs);
 
 
-// notice the lack of parantheses around the "profileDataArr" parameter?
+// const [name, github] = profileDateArgs;
 
-const printProfileData = profileDataArr => {
-    for (let i =0; i < profileDataArr.length; i++) {
 
-    
-    
-    console.log(profileDataArr[i]);
-}
-};
+// // // notice the lack of parantheses around the "profileDataArr" parameter?
 
-printProfileData(profileDateArgs);
+// // const printProfileData = profileDataArr => {
+// //     //this...
+
+// //     for (let i =0; i < profileDataArr.length; i += 1) {
+// //         console.log(profileDataArr[i]);
+// // }
+
+// // console.log('===================');
+
+// // //is the same as this...
+// // profileDataArr.forEach(profileItem => 
+// //     console.log(profileItem));
+// // };
+
+// //const profileDataArgs = process.argv.slice(2);
+
+// // const printProfileData = profileDataArr => {
+// //   // This...
+// //   for (let i = 0; i < profileDataArr.length; i += 1) {
+// //     console.log(profileDataArr[i]);
+// //   }
+
+// //   console.log('================');
+
+// //   // Is the same as this...
+// //   profileDataArr.forEach(profileItem => console.log(profileItem));
+// // };
+
+// // printProfileData(profileDataArgs);
+
+
+//     const generatePage = (name, github) => {
+//         return `
+//         <!DOCTYPE html> 
+//         <html lang="en"> 
+//         <head>
+//           <meta charset="UTF-8">
+//           <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//           <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//           <title>Portfolio Demo</title>
+//         </head>
+      
+//         <body>
+//           <h1>${name}</h1>
+//           <h2><a href="https://github.com/${github}">Github</a></h2>
+//         </body>
+//         </html>
+//         `;
+//       };
+
+
+
+//       fs.writeFile('index.html', generatePage(name, github), err => {
+//         if (err) throw err;
+      
+//         console.log('Portfolio complete! Check out index.html to see the output!');
+//       });
+
+
+
+const fs = require('fs');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+const generatePage = require("./src/page-template");
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+
+  console.log(name, github);
+
+});
+
+//what's the difference in this line?
+// fs.writeFile('./index.html', pageHTML, err => {
+  // if (err) throw err;
+
